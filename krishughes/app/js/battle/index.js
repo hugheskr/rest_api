77664e4221b1079e5511345c2d1f73c51193c4f4
Kris <kristopher_hughes@yahoo.com>
2016-04-27 12:13:28 -0700

    added battle to view and to character contr

--- krishughes/app/js/battle/index.js
+++
@@ -1,3 +0,0 @@
-module.exports = function(app) {
-  require('./controllers/battle_controller')(app);
-};
--- krishughes/app/js/characters/controllers/characters_controller.js
+++ krishughes/app/js/characters/controllers/characters_controller.js
@@ -9,6 +9,8 @@ module.exports = function(app) {
       $scope.heroService = Resource('/heroes');
       $scope.villainService = Resource('/villains');
       $scope.errors = [];
+      $scope.battleService = Resource('/battle');
+      $scope.results = '';
 
       $scope.dismissError = function(err) {
         $scope.errors.splice($scope.errors.indexOf(err), 1);
@@ -22,6 +24,13 @@ module.exports = function(app) {
           character.backup = angular.copy(character);
           character.editing = true;
         }
+      };
+
+      $scope.getBattle = function() {
+        battleService.getAll(function(err, res) {
+          if (err) return console.log(err);
+          $scope.results = res;
+        });
       };
 
       $scope.getAllCharacters = function(service, list) {
